725c9558ad3b0a9eeafc8486b36dabb885c44605
Hamdani Ilham <d41113512@gmail.com>
2025-07-06 23:59:07 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="CetakCerdas.com membantu Anda menghitung harga cetak dokumen, print, dan jilid secara online. Mudah, cepat, dan akurat.">
+        <meta name="keywords" content="cetak cerdas,cetak online, harga print, harga jilid, cetak dokumen, print pdf, hitung biaya cetak">
+        <link rel="canonical" href="https://cetakcerdas.com/" />
+        <meta name="author" content="dhank77 | PT. Hitech Aksara Digital">
 
         <script>
             (function() {

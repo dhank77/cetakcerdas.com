ad5971ec6efde1f6d7d2ea30929b1d171a17b509
Hamdani Ilham <d41113512@gmail.com>
2025-07-13 20:54:11 +0800

    Update user-menu-content.tsx

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -14,7 +14,7 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
 
     const handleLogout = () => {
         cleanup();
-        router.post(route('logout'));
+        router.flushAll();
     };
 
     return (
